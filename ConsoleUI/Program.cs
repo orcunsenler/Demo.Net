50c437b66bec76734352a03e2274c58d74416084
Orçun Şenler <orcunsenler@gmail.com>
2022-09-08 16:43:03 +0300

    Institutive Structure

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,10 +2,21 @@
 using Business.Concrete;
 using System;
 using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 
 class Program
 {
     static void Main(string[] args)
+    {
+        //ProductTest();
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        foreach (var category in categoryManager.GetAll())
+        {
+            Console.WriteLine(category.CategoryName);
+        }
+    }
+
+    private static void ProductTest()
     {
         ProductManager productManager = new ProductManager(new EfProductDal());
 
@@ -21,6 +32,5 @@ class Program
         //{
         //    Console.WriteLine(product.ProductName);
         //}
-
     }
 }
---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,63 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext> : IEntityRepository<TEntity>
+        where TEntity : class,IEntity,new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())  //IDisposable pattern implamentation of c#
+            {
+                var addProduct = context.Entry(entity);
+                addProduct.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deleteProduct = context.Entry(entity);
+                deleteProduct.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public List<TEntity> GelAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updateProduct = context.Entry(entity);
+                updateProduct.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
