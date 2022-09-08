122a542f475203c8e8aab8d6ff0f71361dbee2c6
senler <senler@DESKTOP-070V8R0>
2022-09-08 12:46:54 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Concrete.InMemory;
+using Business.Concrete;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+        foreach (var product in productManager.GetAll())
+        {
+            Console.WriteLine(product.ProductName);
+        }
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,79 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ ProductId=1,  CategoryId=1, ProductName="computer", UnitInStock=10, UnitPrice=100},
+                new Product{ ProductId=2,  CategoryId=2, ProductName="keyboard", UnitInStock=20, UnitPrice=10},
+                new Product{ ProductId=3,  CategoryId=2, ProductName="mouse", UnitInStock=20, UnitPrice=10},
+                new Product{ ProductId=4,  CategoryId=1, ProductName="monitor", UnitInStock=10, UnitPrice=50},
+                new Product{ ProductId=5,  CategoryId=2, ProductName="mousepad", UnitInStock=20, UnitPrice=5}
+            }.ToList();
+        }
+
+        public List<Product> GelAll()
+        {
+            return _products;
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //if I didn't use LINQ(Language Integrated Query), I would write as below
+            //var productToDelete;
+            //foreach (var item in _products)
+            //{
+            //    if(item.ProductId == product.ProductId)
+            //    {
+            //        productToDelete = item;
+            //    }
+            //}
+
+            
+            var productToDelete = _products.SingleOrDefault(x=>x.ProductId==product.ProductId); //by using LINQ
+            _products.Remove(productToDelete);
+        }
+
+        
+        public void Update(Product product)
+        {
+            var productToUpdate = _products.SingleOrDefault(x => x.ProductId == product.ProductId); //by using LINQ
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.UnitPrice = product.UnitPrice;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            _products.Where(x => x.CategoryId == categoryId).ToList();
+            return _products;
+        }
+
+        public List<Product> GelAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
