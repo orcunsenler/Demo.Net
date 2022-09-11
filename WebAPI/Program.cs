b0475f5fa975e223d8b653ac982e475ce28dada3
Orçun Şenler <orcunsenler@gmail.com>
2022-09-11 16:00:44 +0300

    Autofac implementation

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolver.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Microsoft.EntityFrameworkCore;
@@ -7,12 +10,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.AddTransient<IProductService,ProductManager>();
-builder.Services.AddTransient<IProductDal, EfProductDal>();
+//builder.Services.AddTransient<IProductService, ProductManager>();          //If we don't use IoC container like autofac etc., we use this method 
+//builder.Services.AddTransient<IProductDal, EfProductDal>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
