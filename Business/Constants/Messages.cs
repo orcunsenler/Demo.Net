699dc06725b2464c37863b155a5d8d45badec6cb
Orçun Şenler <orcunsenler@gmail.com>
2022-09-13 20:46:59 +0300

    JWT

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "The Category can only have 10 products";
         public static string ProductNameAlreadyExistError = "The product name already exists";
         public static string CategoryLimitExceeded = "The category limit exceeded";
+        public static string AuthorizationDenied = "You don't have authorization";
+        public static string UserRegistered = "User registered";
+        public static string UserNotFound ="Registered user not found";
+        public static string PasswordError = "Password Error";
+        public static string SuccessfulLogin = "Login Successful";
+        public static string UserAlreadyExists = "The User already exists";
+        public static string AccessTokenCreated ="Token was created";
     }
 }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,17 @@
 using Autofac;
+using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
 using Business.DependencyResolver.Autofac;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +19,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 //builder.Services.AddTransient<IProductService, ProductManager>();          //If we don't use IoC container like autofac etc., we use this method 
 //builder.Services.AddTransient<IProductDal, EfProductDal>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["TokenOptions:Issuer"],                      //tokenOptions.Issuer,
+            ValidAudience = builder.Configuration["TokenOptions:Audience"],                    //tokenOptions.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(builder.Configuration["TokenOptions:SecurityKey"])
+        };
+    });
 
 var app = builder.Build();
 
@@ -29,6 +51,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
